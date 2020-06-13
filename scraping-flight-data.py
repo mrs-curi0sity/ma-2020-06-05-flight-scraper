de77e0abbc95b69f04d1285e967cbcc9836e0013
ma <aretzlena@gmail.com>
2020-06-13 12:21:11 +0200

    added path and config for persistence on aws s3 bucket

--- scraping-flight-data.py
+++ scraping-flight-data.py
@@ -6,16 +6,24 @@ from dash.dependencies import Input, Output
 
 import plotly.graph_objs as go
 
-import os.path
+import io
 import requests
 from datetime import datetime
 import pandas as pd
 
 import dash_auth
 
+import logging
+import boto3
+from botocore.exceptions import ClientError
 
+# aws s3 config
+s3_client = boto3.client('s3')
+BUCKET_NAME = 'ma-2020-06-flight-scraper'
+FILE_NAME = 'flight_count.csv'
+
+# dash credentials
 USERNAME_PASSWORD_PAIRS = [['peter', 'lustig'], ['schnett', 'schnoo']]
-
 app = dash.Dash()
 auth = dash_auth.BasicAuth(app, USERNAME_PASSWORD_PAIRS)
 
@@ -24,14 +32,15 @@ server = app.server
 
 # scrape every 30 seconds
 INTERVAL_IN_MS = 30000
-flight_count_path = 'flight_count.csv'
+
+
 
 app.layout = html.Div([
     html.H1('Scraping flight data'),
     html.Div([
         html.Iframe(src = 'https://www.flightradar24.com/',
-                   width = 1200, 
-                    height = 500)
+                width = 1200, 
+                height = 500)
     ]),
     html.Div([
         html.Pre(id = 'counter-text',
@@ -44,20 +53,16 @@ app.layout = html.Div([
     ])
 ])
 
-# file already exists
-if os.path.isfile(flight_count_path):
-    df_previous_flight_count = pd.read_csv(flight_count_path, parse_dates=['timestamp'],  index_col = False)
-# first recording:
-else:
-    df_previous_flight_count = pd.DataFrame(columns = ['timestamp', 'active_flights_count'])
-
-timestamp_list = list(df_previous_flight_count['timestamp'])
-counter_list = list(df_previous_flight_count['active_flights_count'])
-
 
 @app.callback(Output('counter-text', 'children'),
              [Input('interval-component', 'n_intervals')])
 def update_flights(n):
+    # read in previous data
+    obj = s3_client.get_object(Bucket= BUCKET_NAME , Key = FILE_NAME)
+    df_previous_flight_count = pd.read_csv(io.BytesIO(obj['Body'].read()), encoding='utf8', index_col = False)
+    df_previous_flight_count.columns = ['timestamp', 'active_flights_count']
+    
+    # scrape count
     url = "https://data-live.flightradar24.com/zones/fcgi/feed.js?faa=1\
            &mlat=1&flarm=1&adsb=1&gnd=1&air=1&vehicles=1&estimated=1&stats=1"
     res = requests.get(url, headers = {'User-Agent': 'Mozilla/5.0'})
@@ -65,13 +70,19 @@ def update_flights(n):
     counter = 0
     for element in data["stats"]["total"]:
         counter += data["stats"]["total"][element]
-    counter_list.append(counter)
-    timestamp_list.append(datetime.now())
-    print(f'[INFO] {len(counter_list)} now: {timestamp_list[-1]}, counter: {counter}')
-
-    df_flight_counter_current = pd.DataFrame({'timestamp': timestamp_list, 'active_flights_count': counter_list})
-    df_flight_count_all = pd.concat([df_previous_flight_count, df_flight_counter_current]).sort_values(by = 'timestamp').drop_duplicates()
-    df_flight_count_all.to_csv(flight_count_path, index = False)
+    
+    # add line to df_flight_count_all
+    df_flight_counter_current = pd.DataFrame({'timestamp': [datetime.now()], 'active_flights_count': counter})
+    df_flight_count_all = pd.concat([df_previous_flight_count, df_flight_counter_current])#.sort_values(by = 'timestamp').drop_duplicates()
+    
+    print(f'[INFO previous] {df_previous_flight_count.tail(2)}')
+    print(f'\n[INFO current] {df_flight_counter_current}')
+    print(f'\n[INFO all] {df_flight_count_all.tail(2)}')
+    df_flight_count_all.to_csv('flight_count_backup_real_data_is_on_s3.csv', index = False)
+    
+    print('[INFO] writing to S3')
+    # df_flight_count_all.to_csv(flight_count_path, index = False)
+    df_flight_count_all.to_csv(f's3://{BUCKET_NAME}/{FILE_NAME}', index = False)
     
     return f'active flights right now: {counter}'
 
@@ -79,12 +90,14 @@ def update_flights(n):
 @app.callback(Output('graph-number-of-flights', 'figure'),
              [Input('interval-component', 'n_intervals')])
 def update_graph(n):
-    df_flight_count_all = pd.read_csv(flight_count_path).sort_values(by = 'timestamp')
+    # df_flight_count_all = pd.read_csv(flight_count_path).sort_values(by = 'timestamp')
+    obj = s3_client.get_object(Bucket= BUCKET_NAME , Key = FILE_NAME)
+    flight_count = pd.read_csv(io.BytesIO(obj['Body'].read()), encoding='utf8')
+    
     figure = go.Figure(
         data = [go.Scatter(
-            x = df_flight_count_all['timestamp'],
-            #x = list(range(len(counter_list))),
-            y = df_flight_count_all['active_flights_count'],
+            x = flight_count['timestamp'],
+            y = flight_count['active_flights_count'],
             mode = 'lines+markers'
         )]
     ) 
