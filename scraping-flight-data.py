58fa3ac543900e43b90603c96272e76d5cc2b09a
ma <aretzlena@gmail.com>
2020-06-20 00:16:14 +0200

    restructured intervals in order to avoid Heroku H12 request timeouts

--- scraping-flight-data.py
+++ scraping-flight-data.py
@@ -27,9 +27,8 @@ print(f'AWS_SECRET_ACCESS_KEY: {os.environ["AWS_SECRET_ACCESS_KEY"]}')
 s3_client = boto3.client('s3')
 BUCKET_NAME = 'ma-2020-06-flight-scraper'
 FILE_NAME = 'flight_count.csv'
-SCRAPE_INTERVAL_IN_SECONDS = 60 # scrape every minute
-SAVE_INTERVAL_IN_SECONDS = 600 # only save every 10 minutes
-OFFSET_IN_SECONDS = 5 # time buffer to save crawled data to S3
+UPDATE_INTERVAL_IN_SECONDS = 15 # this has to be smaller than 30 in order to avoid Heroku H12 - Request timeout error
+SAVE_INTERVAL_IN_MINUTES = 5 # only save every 10 minutes
 
 # dash credentials
 USERNAME_PASSWORD_PAIRS = [['peter', 'lustig'], ['schnett', 'schnoo']]
@@ -50,7 +49,7 @@ app.layout = html.Div([
         html.Pre(id = 'counter-text',
                 children = f'Number of flights right now ({datetime.now()})'),
         dcc.Interval(id = 'interval-component',
-                    interval = SAVE_INTERVAL_IN_SECONDS * 1000,  # milliseconds
+                    interval = UPDATE_INTERVAL_IN_SECONDS * 1000,  # milliseconds
                     n_intervals = 0),
         dcc.Graph(id = 'graph-number-of-flights', 
                  style = {'widht': 1200})
@@ -72,40 +71,34 @@ def scrape_count():
              [Input('interval-component', 'n_intervals')])
 def update_flights(n):
     start_time = datetime.now()
-    end_time = start_time + timedelta(seconds = SAVE_INTERVAL_IN_SECONDS - OFFSET_IN_SECONDS)
-    # read in previous data
-    obj = s3_client.get_object(Bucket= BUCKET_NAME , Key = FILE_NAME)
-    df_previous_flight_count = pd.read_csv(io.BytesIO(obj['Body'].read()), encoding='utf8', index_col = False)
-    df_previous_flight_count.columns = ['timestamp', 'active_flights_count']
-
-    # scrape every 30 seconds
-    # but only save after 5 minutes
-    now = datetime.now()
     time_list = []
     counter_list = []
-    while now < end_time:
-        now = datetime.now()
-        counter = scrape_count()
+    counter = scrape_count()
+    
+    # only write every 5 minutes
+    if start_time.minute % SAVE_INTERVAL_IN_MINUTES == 0:
+    
+        # read in previous data
+        obj = s3_client.get_object(Bucket= BUCKET_NAME , Key = FILE_NAME)
+        df_previous_flight_count = pd.read_csv(io.BytesIO(obj['Body'].read()), encoding='utf8', index_col = False)
+        df_previous_flight_count.columns = ['timestamp', 'active_flights_count']
+
         time_list.append(now)
         counter_list.append(counter)
         print(f'[INFO] fetched new data: {now}, flight count is {counter}. Will continue iteration until time is {end_time}')
-        print(f'[INFO] ... sleeping for {SCRAPE_INTERVAL_IN_SECONDS} seconds')
-        time.sleep(SCRAPE_INTERVAL_IN_SECONDS)
         
-        
-    # add line to df_flight_count_all
-    df_flight_counter_current = pd.DataFrame({'timestamp': time_list, 'active_flights_count': count_list})
-    df_flight_count_all = pd.concat([df_previous_flight_count, df_flight_counter_current])#.sort_values(by = 'timestamp').drop_duplicates()
+        # add line to df_flight_count_all
+        df_flight_counter_current = pd.DataFrame({'timestamp': time_list, 'active_flights_count': count_list})
+        df_flight_count_all = pd.concat([df_previous_flight_count, df_flight_counter_current])#.sort_values(by = 'timestamp').drop_duplicates()
+
+        print(f'[INFO previous] {df_previous_flight_count.tail(2)}')
+        print(f'\n[INFO current] {df_flight_counter_current}')
+        print(f'\n[INFO all] {df_flight_count_all.tail(2)}')
+        # df_flight_count_all.to_csv('flight_count_backup_real_data_is_on_s3.csv', index = False)
     
-    print(f'[INFO previous] {df_previous_flight_count.tail(2)}')
-    print(f'\n[INFO current] {df_flight_counter_current}')
-    print(f'\n[INFO all] {df_flight_count_all.tail(2)}')
-    # df_flight_count_all.to_csv('flight_count_backup_real_data_is_on_s3.csv', index = False)
+        print('[INFO] writing to S3')
+        df_flight_count_all.to_csv(f's3://{BUCKET_NAME}/{FILE_NAME}', index = False)
     
-    print('[INFO] writing to S3')
-    
-    # TODO EINKOMMENTIEREN
-    df_flight_count_all.to_csv(f's3://{BUCKET_NAME}/{FILE_NAME}', index = False)
     
     return f'active flights right now: {counter}'
 
